592b3968c3a6c4cc075a3917d9356bdb02d2a7b0
Meryem-Boudri <boudrimeryem11@gmail.com>
2024-03-18 01:24:21 +0000

    first commit

--- src/main/java/ma/enset/jpatp2/entities/Patient.java
+++ src/main/java/ma/enset/jpatp2/entities/Patient.java
@@ -2,11 +2,15 @@ package ma.enset.jpatp2.entities;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
 @Entity
@@ -14,8 +18,11 @@ import java.util.Date;
 public class Patient {
     @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty
+    @Size(min=4,max = 40)
     private String nom;
     @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date date;
     private boolean malade;
     @OneToMany(mappedBy = "patient")
--- src/main/java/ma/enset/jpatp2/web/PatientController.java
+++ src/main/java/ma/enset/jpatp2/web/PatientController.java
@@ -1,5 +1,6 @@
 package ma.enset.jpatp2.web;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import ma.enset.jpatp2.entities.Patient;
 import ma.enset.jpatp2.repository.PatientRepository;
@@ -8,7 +9,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -29,18 +32,54 @@ public class PatientController {
                         String keyword) {
 
 
-        Page<Patient> patientPage = patientRepository.findByNomContains(keyword,PageRequest.of(page,size));
+        Page<Patient> patientPage = patientRepository.findByNomContains(keyword, PageRequest.of(page, size));
         model.addAttribute("listPatients", patientPage.getContent());
         model.addAttribute("pages", new int[(int) patientPage.getTotalElements()]);//tableau avec dimension du nombre de page
         model.addAttribute("currentPage", page);//page actuel
-        model.addAttribute("keyword",keyword);
+        model.addAttribute("keyword", keyword);
         return "patients";
     }
+
     @GetMapping("/delete")
-    public String delete(Long id,String kw,int page){
+    public String delete(Long id, String kw, int page) {
         patientRepository.deleteById(id);
-       return "redirect:/index?page="+page+"&keyword"+kw; //redirection
+        return "redirect:/index?page=" + page + "&keyword" + kw; //redirection
     }
 
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/index"; //redirection
+    }
+
+    @GetMapping("/formPatients")
+    public String formPatients(Model model) {
+        model.addAttribute("patient", new Patient());
+        return "formPatients";
+    }
+
+    @PostMapping(path = "/save")
+    public String save(Model model, @Valid Patient patient, BindingResult bindingResult,
+                       @RequestParam(name = "page", defaultValue = "0") int page,
+                       @RequestParam(name = "keyword", defaultValue = "") String kw
+                       ) { //Stocker les errors dans bindingResult
+
+        if (bindingResult.hasErrors()) return "formPatients";
+        patientRepository.save(patient);
+        return "redirect:/index?page="+page+"&keyword="+kw; //mieux de construire nouvelle page pour dire au utilisateur c'est bien saisi
+//etapes de validation: ajouter la dependace - ajouter les annotations de validation "beans" - @valid et bindingResult "controller" - th errors "html"
+    }
+
+    @GetMapping("/editPatient")
+    public String editPatient(Model model, Long id , String keyword, int page ) {
+        Patient patient=patientRepository.findById(id).orElse(null);
+        if(patient==null){
+            throw  new RuntimeException("Patient introuvale");
+        }
+        model.addAttribute("patient", patient);
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("page", page);
+
+        return "editPatient";
+    }
 
 }
